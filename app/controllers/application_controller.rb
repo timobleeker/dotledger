cb90601f3ebb053bb9cb0d7bc71796ca49ba2ab6
Kale Worsley <kale@worsley.co.nz>
2014-06-21 18:30:36 +1200

    Add http basic authentication.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  if Rails.application.secrets.enable_authentication
+    http_basic_authenticate_with(
+      name: Rails.application.secrets.username,
+      password: Rails.application.secrets.password
+    )
+  end
+
   def boot
   end
 end
